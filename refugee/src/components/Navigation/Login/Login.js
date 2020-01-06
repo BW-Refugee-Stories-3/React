dbecbfd2005ba88c7b6ff97c3139bb989faf8369
doren <56702391+doranium2013@users.noreply.github.com>
2020-01-06 14:07:22 -0800

    created form

--- refugee/src/components/Navigation/Login/Login.js
+++ refugee/src/components/Navigation/Login/Login.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export default function Login() {
     return (
         <div>
-            login works
+           <form>
+               <label htmlFor="userNameInput">Username: </label> 
+               <input 
+               maxLength="20"
+               placeholder="Username Here"
+               id="userNameInput"
+               name="username"
+               type="text"
+               />
+
+            <form>
+                <label htmlFor="userNamePassword">Password: </label>
+                <input 
+               maxLength="20"
+               placeholder="Password Here"
+               id="userNamePassword"
+               name="password"
+               type="password"
+               />
+               
+               </form>  
+           </form>
+           <button>Submit</button>
+           <button>Register</button>
         </div>
     )
 }
